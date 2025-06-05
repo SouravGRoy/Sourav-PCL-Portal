8b5d693b66ae0cbdcb3645a0790bf17dd7a6b3f4
itsdrvgo <thedragoluca@gmail.com>
2025-06-05 20:28:37 +0530

    Update ESLint configuration to customize rules for TypeScript and JavaScript
    - Disabled semicolon enforcement
    - Added warning for the use of 'any' type in TypeScript
    - Turned off unused variable checks for TypeScript and JavaScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            semi: "off",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": "off",
+            "no-unused-vars": ["off", "always"],
+        },
+    },
 ];
 
 export default eslintConfig;
